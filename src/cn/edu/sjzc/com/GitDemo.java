e4ccf33e7515efc07c6b52d67d04f8cac16d299f
EvainWs <evain_wang@foxmail.com>
2022-03-28 22:53:52 +0800

    first commit

---
+++ src/cn/edu/sjzc/com/GitDemo.java
@@ -0,0 +1,11 @@
+package cn.edu.sjzc.com;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("-----begin");
+        for (int i=0;i<10;i++){
+            System.out.println("当前是第几次+----"+i);
+        }
+        System.out.println("-----end");
+    }
+}
